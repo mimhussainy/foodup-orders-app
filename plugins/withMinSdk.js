5e8ad8264ffc141f4a48f183b116ab5bd8ba2215
Mohammad <mimhussainy@gmail.com>
2026-05-11 20:38:57 +0200

    Clean build - bluetooth printer coming soon

--- plugins/withMinSdk.js
+++ plugins/withMinSdk.js
@@ -1,25 +1,3 @@
-const { withAppBuildGradle, withAndroidManifest } = require('@expo/config-plugins');
-
 module.exports = function withMinSdk(config) {
-  config = withAppBuildGradle(config, (config) => {
-    config.modResults.contents = config.modResults.contents.replace(
-      'minSdkVersion rootProject.ext.minSdkVersion',
-      'minSdkVersion 26'
-    );
-    return config;
-  });
-
-  config = withAndroidManifest(config, (config) => {
-    const manifest = config.modResults.manifest;
-    manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
-    manifest['uses-sdk'] = [{
-      '$': {
-        'android:minSdkVersion': '26',
-        'tools:overrideLibrary': 'com.goodcom.react.EzPrinter'
-      }
-    }];
-    return config;
-  });
-
   return config;
 };
