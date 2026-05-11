f726d92daa5ef0803faf268f09d9e25b2f0aa74d
Mohammad <mimhussainy@gmail.com>
2026-05-11 19:28:21 +0200

    Fix both minSdk and manifest

--- plugins/withMinSdk.js
+++ plugins/withMinSdk.js
@@ -1,11 +1,30 @@
-const { withAppBuildGradle } = require('@expo/config-plugins');
+const { withAppBuildGradle, withAndroidManifest } = require('@expo/config-plugins');
 
 module.exports = function withMinSdk(config) {
-  return withAppBuildGradle(config, (config) => {
+  // Fix build.gradle
+  config = withAppBuildGradle(config, (config) => {
     config.modResults.contents = config.modResults.contents.replace(
       'minSdkVersion rootProject.ext.minSdkVersion',
       'minSdkVersion 26'
     );
     return config;
   });
+
+  // Fix AndroidManifest.xml
+  config = withAndroidManifest(config, (config) => {
+    const manifest = config.modResults.manifest;
+    if (!manifest['uses-sdk']) {
+      manifest['uses-sdk'] = [];
+    }
+    manifest['uses-sdk'] = [{
+      '$': {
+        'xmlns:tools': 'http://schemas.android.com/tools',
+        'android:minSdkVersion': '26',
+        'tools:overrideLibrary': 'com.goodcom.react.EzPrinter'
+      }
+    }];
+    return config;
+  });
+
+  return config;
 };
